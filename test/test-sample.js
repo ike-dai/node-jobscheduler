e8cba29854911ef9e5dd5e96214f60e2726b6bd2
ike-dai <dai.ikd123@gmail.com>
2016-10-08 02:31:51 +0900

    add jobchain library

--- test/test-sample.js
+++ test/test-sample.js
@@ -37,3 +37,17 @@ describe('Job', function () {
 		});
 	});
 });
+
+describe('JobChain', function () {
+	describe('start(add order) success test: ' +  process.env.JSTESTJOBCHAIN, function () {
+		it('成功時、結果のxmlにokが返ってくること', function (done) {
+			client = new jobscheduler({url: process.env.JSTESTURL});
+			client.jobchain.start({jobchain: process.env.JSTESTJOBCHAIN}, function(err, res, result){
+				//console.log(result.spooler.answer.ok);
+				res.statusCode.should.equal(200);
+				result.answer.should.have.property('ok');
+				done();
+			});
+		});
+	});
+});
