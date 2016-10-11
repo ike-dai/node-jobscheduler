760f9d056882a7705bf08ed22c342c1fa85dedfb
ike-dai <dai.ikd123@gmail.com>
2016-10-11 13:09:31 +0900

    add modify job func

--- test/test-sample.js
+++ test/test-sample.js
@@ -36,6 +36,56 @@ describe('Job', function () {
 			});
 		});
 	});
+	describe('modify job success test: ' +  process.env.JSTESTJOB, function () {
+		it('stopへの変更成功時、結果のxmlにokが返ってくること', function (done) {
+			client = new jobscheduler({url: process.env.JSTESTURL});
+			client.job.modify_job({job: process.env.JSTESTJOB, cmd: 'stop'}, function(err, res, result){
+				res.statusCode.should.equal(200);
+				result.answer.should.have.property('ok');
+				done();
+			});
+		});
+	});
+	describe('stop job success test: ' +  process.env.JSTESTJOB, function () {
+		it('成功時、結果のxmlにokが返ってくること', function (done) {
+			client = new jobscheduler({url: process.env.JSTESTURL});
+			client.job.stop({job: process.env.JSTESTJOB}, function(err, res, result){
+				res.statusCode.should.equal(200);
+				result.answer.should.have.property('ok');
+				done();
+			});
+		});
+	});
+	describe('unstop job success test: ' +  process.env.JSTESTJOB, function () {
+		it('成功時、結果のxmlにokが返ってくること', function (done) {
+			client = new jobscheduler({url: process.env.JSTESTURL});
+			client.job.unstop({job: process.env.JSTESTJOB}, function(err, res, result){
+				res.statusCode.should.equal(200);
+				result.answer.should.have.property('ok');
+				done();
+			});
+		});
+	});
+	describe('suspend job success test: ' +  process.env.JSTESTJOB, function () {
+		it('成功時、結果のxmlにokが返ってくること', function (done) {
+			client = new jobscheduler({url: process.env.JSTESTURL});
+			client.job.suspend({job: process.env.JSTESTJOB}, function(err, res, result){
+				res.statusCode.should.equal(200);
+				result.answer.should.have.property('ok');
+				done();
+			});
+		});
+	});
+	describe('wake job success test: ' +  process.env.JSTESTJOB, function () {
+		it('成功時、結果のxmlにokが返ってくること', function (done) {
+			client = new jobscheduler({url: process.env.JSTESTURL});
+			client.job.wake({job: process.env.JSTESTJOB}, function(err, res, result){
+				res.statusCode.should.equal(200);
+				result.answer.should.have.property('ok');
+				done();
+			});
+		});
+	});
 });
 
 describe('JobChain', function () {
